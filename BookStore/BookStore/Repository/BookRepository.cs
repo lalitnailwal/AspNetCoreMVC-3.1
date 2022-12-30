7db64ffefd6fb4a489b9604d0a00a1c4e8550d58
lalitnailwal <lalitnailwal@hotmail.com>
2022-12-30 11:34:48 +0530

    Addition of Get Book page along with viewstart file

--- BookStore/BookStore/Repository/BookRepository.cs
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -25,12 +25,12 @@ namespace BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id =1, Title = "MVC", Author ="Lalit", Description = "This is a description for MVC book"},
-                new BookModel(){Id =2, Title = "Dot Net Core", Author ="Prema", Description = "This is a description for Dot Net Core book"},
-                new BookModel(){Id =3, Title = "C#", Author ="Akshat", Description = "This is a description for C# book"},
-                new BookModel(){Id =4, Title = "Java", Author ="Jeevan", Description = "This is a description for Java book"},
-                new BookModel(){Id =5, Title = "Php", Author ="Kailash", Description = "This is a description for Php book"},
-                new BookModel(){Id =5, Title = "Azure Devops", Author ="Nitin", Description = "This is a description for Azure Devops book"}
+                new BookModel(){Id =1, Title = "MVC", Author ="Lalit", Description = "This is a description for MVC book", Category="Programming", Language="English", TotalPages=134 },
+                new BookModel(){Id =2, Title = "Dot Net Core", Author ="Prema", Description = "This is a description for Dot Net Core book", Category="Framework", Language="English", TotalPages=567 },
+                new BookModel(){Id =3, Title = "C#", Author ="Akshat", Description = "This is a description for C# book", Category="Developer", Language="Hindi", TotalPages=897 },
+                new BookModel(){Id =4, Title = "Java", Author ="Jeevan", Description = "This is a description for Java book", Category="Concept", Language="English", TotalPages=564 },
+                new BookModel(){Id =5, Title = "Php", Author ="Kailash", Description = "This is a description for Php book", Category="Programming", Language="English", TotalPages=100 },
+                new BookModel(){Id =5, Title = "Azure Devops", Author ="Nitin", Description = "This is a description for Azure Devops book", Category="Devops", Language="English", TotalPages=134 }
             };
         }
     }
